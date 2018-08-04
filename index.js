98903f4a92fd9e25fc166585b882537814234bfe
Horace Nelson <Horace@Horaces-MacBook-Air.local>
2018-08-03 20:23:33 -0400

    Initial release

---
+++ index.js
@@ -0,0 +1,110 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+import fetch from 'node-fetch'
+import Webtask from 'webtask-tools'
+
+const server = express()
+
+let accessToken
+let lastTokenFetch = 0
+
+const getAccessToken = (req, res, next) => {
+  const context = req.webtaskContext
+
+  context.storage.get(async (error, data = {}) => {
+    if (error) {
+      next(error)
+      return
+    }
+    const { accessToken, lastTokenFetch } = data
+    const canUseToken = lastTokenFetch && (Date.now() - lastTokenFetch) / 1000 / 60 < 30
+
+    if (accessToken && canUseToken) { req.accessToken = accessToken }
+
+    const { AUTH0_DOMAIN, MGMT_CLIENT_ID, MGMT_CLIENT_SECRET } = context.data
+    const url = `https://${AUTH0_DOMAIN}/oauth/token`
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        'client_id': MGMT_CLIENT_ID,
+        'client_secret': MGMT_CLIENT_SECRET,
+        'audience': `https://${AUTH0_DOMAIN}/api/v2/`,
+        'grant_type': 'client_credentials'
+      })
+    }
+
+    await fetch(url, options)
+      .then(data => data.json())
+      .then(data => data.access_token)
+      .then(accessToken => {
+        req.accessToken = accessToken
+        context.storage.set({
+          accessToken,
+          lastTokenFetch: Date.now()
+        }, (error) => {
+          if (error) { /* this error is pretty irrelevant */ }
+        })
+        next()
+      })
+  })
+}
+
+const getIdpToken = (req, res, next) => {
+  if (!req.user || !req.accessToken) {
+    next()
+    return
+  }
+
+  const { AUTH0_DOMAIN } = req.webtaskContext.data
+  const { sub } = req.user
+  const subParts = sub.split('|')
+  const accessToken = req.accessToken
+  const url = `https://${AUTH0_DOMAIN}/api/v2/users/${sub}`
+
+  req.provider = subParts[0]
+  req.userId = subParts[1]
+
+  const options = {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${accessToken}`
+    }
+  }
+
+  fetch(url, options)
+    .then(data => data.json())
+    .then(data => {
+      const { access_token = null } = data.identities.find(({provider}) => provider === req.provider)
+      req.idpToken = access_token
+      next()
+    })
+}
+
+const callIdp = (req, res, next) => {
+  const apiUrl = req.body.apiUrl
+  const idpToken = req.idpToken
+  const options = {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${idpToken}`
+    }
+  }
+
+  fetch(apiUrl, options)
+    .then(data => data.json())
+    .then(data => {
+      res.json(data)
+    })
+}
+
+server
+  .use(bodyParser.json())
+  .post(
+    '*',
+    getAccessToken,
+    getIdpToken,
+    callIdp
+  )
+
+module.exports = Webtask.fromExpress(server).auth0()
